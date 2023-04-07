50c0683691d6cc31f78aef2d021036b4663e7afc
soumya <117825720+soumya3969@users.noreply.github.com>
2023-04-08 02:09:46 +0530

    complex number calculation using java

---
+++ Basic/Complex.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Complex {
+    int real;
+    int img;
+    public void input(int x,int y)
+    {
+        real=x;
+        img =y;
+    }
+    public
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        Complex t1 = new Complex();
+        Complex t2 = new Complex();
+        Complex sum = new Complex();
+        Complex sub = new Complex();
+        System.out.println("Enter the real and imaginary components of the 1st complex number");
+        t1.input(sc.nextInt(),sc.nextInt());
+        System.out.println("Enter the real and imaginary components of the 2nd complex number");
+        t2.input(sc.nextInt(),sc.nextInt());
+        // System.out.println("t1.real = " + t1.real + " t2.imag = " + t2.img);
+        int s=t1.real +t2.real;
+        System.out.println(s);
+        switch(choose){
+            case
+        }
+    }
+}
