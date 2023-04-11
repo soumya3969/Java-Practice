814d905a1fe8dc7b9cc80aae3d359849fee6ad06
soumya <117825720+soumya3969@users.noreply.github.com>
2023-04-12 00:58:07 +0530

    divide formula

---
+++ Basic/divide.java
@@ -0,0 +1,9 @@
+class divide{
+    public static void main(String []args){
+        float a=50f;
+        float b=3f;
+        float c;
+        c= a/b;
+        System.out.println("division of " + a + "and " + b + " is " +c);
+    }
+}
