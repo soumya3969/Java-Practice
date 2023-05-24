a94f1c7fdf22d3a17df19fc62249f07cb5e539f1
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-24 05:46:22 +0000

    applet 1

--- Applet/MyFirstApplet.java
+++ Applet/MyFirstApplet.java
@@ -1,7 +1,16 @@
-import java.applet.Applet;
+import java.applet.*;
 import java.awt.*;
 public class MyFirstApplet extends Applet{
     public void init(){
-        setBackground(Color.GREY);
+        setBackground(Color.YELLOW);
+    }
+    public void paint(Graphics g){
+        g.setColor(Color.RED);
+        g.drawString("welcome",50 ,75);
+        Color c = new Color(0,76,200);
+        g.setColor(c);
+        Font f =new Font("Times New Roman" ,Font.BOLD, 50);
+        g.setFont(f);
+        g.drawString("GIFT",100 ,150);
     }
 }
